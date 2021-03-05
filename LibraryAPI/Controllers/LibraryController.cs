fcd2dd465ad0ebd85e8e2435c03cd7b89fdbba5f
Pawxon <zelazek_pawel@wp.pl>
2021-03-05 16:59:05 +0100

    Created LibraryController

---
+++ LibraryAPI/Controllers/LibraryController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LibraryAPI.Models;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/books")]
+    public class LibraryController : ControllerBase
+    {
+        private readonly LibraryDBContext _dbContext;
+        //Dostęp do kontekstu baz danych
+        public LibraryController(LibraryDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+
+        /// <summary>
+        /// Metoda zwracająca wszystkie książki z bazy danych 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<Books>> GetAll()
+        {
+            var books = _dbContext
+                .Books
+                .ToList();
+
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Books> GetById([FromRoute]int id)
+        {
+            var book = _dbContext
+                .Books
+                .FirstOrDefault(r => r.Id == id);
+
+                if(book is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+
+        }
+    }
+}
