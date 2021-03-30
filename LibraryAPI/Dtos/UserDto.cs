1d7ff3a4ccc0a60ae2702d03ab5a0fa66bf75b2a
Pawxon <zelazek_pawel@wp.pl>
2021-03-30 12:04:55 +0200

    Update UserDto.cs

--- LibraryAPI/Dtos/UserDto.cs
+++ LibraryAPI/Dtos/UserDto.cs
@@ -8,7 +8,8 @@ namespace LibraryAPI.Dtos
 {
     public class UserDto
     {
-
+        [Key]
+        public int Id { get; set; }
         [Required]
         public string Name { get; set; }
         [Required]
