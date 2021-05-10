3d73b2b6c9d05e984aaa4e0063ffa986d2eae1f0
Pawxon <zelazek_pawel@wp.pl>
2021-05-10 21:33:39 +0200

    Create UserBookReservationMappingProfile.cs

---
+++ LibraryAPI/UserBookReservationMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using LibraryAPI.Dtos;
+using LibraryAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAPI
+{
+    public class UserBookReservationMappingProfile : Profile
+    {
+        public UserBookReservationMappingProfile()
+        {
+            CreateMap<UserBookReservation, UserBookReservationDto>()
+            .ForMember(m => m.BookName, c => c.MapFrom(s => s.Book.BookName));
+        }
+    }
+}
