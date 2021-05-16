de660ac845e3b71d0fcc416ea25426621123c682
Pawxon <zelazek_pawel@wp.pl>
2021-05-16 20:16:19 +0200

    Update AllBooksReservedByUserDto.cs

--- LibraryAPI/Dtos/AllBooksReservedByUserDto.cs
+++ LibraryAPI/Dtos/AllBooksReservedByUserDto.cs
@@ -11,5 +11,11 @@ namespace LibraryAPI.Dtos
         public DateTime ReservationTime { get; set; }
         public int BookId { get; set; }
         public string BookName { get; set; }
+        public string AuthorName { get; set; }
+        public string PublisherName { get; set; }
+        public int PublishDate { get; set; }
+        public string Category { get; set; }
+        public string Language { get; set; }
+        public string BookDescription { get; set; }
     }
 }
